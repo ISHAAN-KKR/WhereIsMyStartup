3eb0f4591058ab7fbe498aeb060e58583290b2fa
Ishaan Karmakar <116961842+ISHAAN-KKR@users.noreply.github.com>
2025-08-24 11:19:12 +0530

    Update ai_models.py

--- ai_models.py
+++ ai_models.py
@@ -1,6 +1,7 @@
 import pickle
 import numpy as np
 import pandas as pd
+import joblib
 
 def predict_revenue(industry, last_revenue, target_year):
     model_filename = f"./revenuePredictors/{industry}_sarima_model.pkl"
@@ -23,3 +24,27 @@ def predict_revenue(industry, last_revenue, target_year):
     except FileNotFoundError:
         print(f"⚠ Model file not found for {industry}.")
         return None
+
+def predict_profitability(category, revenue, savings):
+    model = joblib.load("./profitMarginPredictor/net_profit_margin_model.pkl")
+    scaler = joblib.load("./profitMarginPredictor/scaler.pkl")
+    label_encoders = joblib.load("./profitMarginPredictor/label_encoders.pkl")
+    selected_features = ['Category', 'Revenue', 'Cash Flow from Operating']
+    categorical_cols = ['Category']
+    
+    user_data = {
+        'Category': category,
+        'Revenue': revenue,
+        'Cash Flow from Operating': savings
+    }
+    
+    user_df = pd.DataFrame([user_data], columns=selected_features)    
+    for col in categorical_cols:
+        if user_df[col].iloc[0] not in label_encoders[col].classes_:
+            label_encoders[col].classes_ = np.append(label_encoders[col].classes_, user_df[col].iloc[0])
+        user_df[col] = label_encoders[col].transform(user_df[col].astype(str))
+    
+    user_scaled = scaler.transform(user_df)
+    prediction = model.predict(user_scaled)[0]
+    
+    return prediction
